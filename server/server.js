0bb527f262eb5d5771b638ac969d6dc9cfbe4a35
yasir19 <yasir.habboo@gmail.com>
2022-03-30 23:29:24 -0500

    get the thought list, set concurrently to run clint side server and back-end server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const { authMiddleware } = require ('./utils/auth')
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -28,7 +29,13 @@ const startServer =  async () => {
  
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+// serve up stativ assets
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+app.get('*', (req,res) => {
+res.sendFile(path.join(__dirname,"../client/build/index.html"))
+});
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
