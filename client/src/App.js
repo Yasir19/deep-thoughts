1f554b9e74466770251594345530b2e298ca8dba
yasir19 <yasir.habboo@gmail.com>
2022-04-03 02:30:58 -0500

    render based on the loggedIn status

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {ApolloProvider, ApolloClient, InMemoryCache , createHttpLink} from '@apollo/client'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// import {setContext} function for apollo client to retrieve the token
+import {setContext} from '@apollo/client/link/context'
 import Login from './pages/Login';
 import NoMatch from'./pages/NoMatch';
 import SingleThought from './pages/SingleThought';
@@ -15,10 +17,21 @@ import Home from './pages/Home';
 const httpLink = createHttpLink({
   uri: '/graphql',
 })
+// middleware function to retrieve the token and combine it with the existing httpLink
+const authLink = setContext((_,{headers}) =>{
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token? `Bearer ${token}`: '',
+    },
+  };
+})
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
+
 function App() {
   return (
     <ApolloProvider client={client}>
