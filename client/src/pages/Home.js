27b03ec45bb02b56d5dcda08ddd6255b1d84ac1c
yasir19 <yasir.habboo@gmail.com>
2022-04-03 11:16:10 -0500

    adding thought"

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import { QUERY_THOUGHTS, QUERY_ME_BASIC} from "../utils/queries";
 import ThoughtList from "../components/ThoughtList";
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
+import ThoughtForm from "../components/ThoughtForm";
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -13,6 +14,13 @@ const { data: userData } = useQuery(QUERY_ME_BASIC);
   const loggedIn = Auth.loggedIn();
   return (
     <main>
+      <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+            </div>
+        )}
+      </div>
       <div className="flex-row justify-space-between">
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
